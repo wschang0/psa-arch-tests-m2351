45ec4f9c523601a949897d9ecf6299ffaf640324
MS20 WSChang0 <wschang0@nuvoton.com>
2019-10-24 11:17:32 +0800

    Split crytpo test due to limited SRAM size.

---
+++ api-tests/crypto6.bat
@@ -0,0 +1,8 @@
+del .\platform\targets\tgt_dev_apis_tfm_m2351\Makefile
+cp .\platform\targets\tgt_dev_apis_tfm_m2351\Makefile_crypto .\platform\targets\tgt_dev_apis_tfm_m2351\Makefile
+
+del .\dev_apis\crypto\testsuite.db
+cp .\dev_apis\crypto\testsuite6.db   .\dev_apis\crypto\testsuite.db
+
+sh ./tools/scripts/setup_unix.sh --target tgt_dev_apis_tfm_m2351 --toolchain ARMCLANG --cpu_arch armv8m_bl --verbose 3 --suite crypto --include ../../trusted-firmware-m/interface/include --archive_tests
+
